6bebca66c78ed238a9bc7e3cecffca7db0f72342
aafco <aafco@LAPTOP-9T8JHAEM>
2022-10-14 13:29:47 -0600

    Agregar archivos de proyecto.

---
+++ L11_AAFC_1162522/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int tipo1, resu;
+            
+            double cant2, dola;
+
+
+
+        Console.WriteLine("Bienvenido ;)");
+
+
+        Console.WriteLine("Ingrese 1 para dolares a euros, ingrese 2 para libras a euros, ingrese 3 para yen a euros");
+        tipo1 = Int32.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Ingrese la cantidad: ");
+        cant2 = Int32.Parse(Console.ReadLine());
+
+    
+     
+
+
+        if ( tipo1 == 1)
+        {
+            Console.WriteLine("Su cantidad es " + cant2 * 0.75);
+        }
+
+        if (tipo1 == 2)
+        {
+            Console.WriteLine("Su cantidad es " + cant2 * 1.22);
+        }
+
+        if (tipo1 == 3)
+        {
+            Console.WriteLine("Su cantidad  es " + cant2 * 0.009);
+        }
+    }
+}
